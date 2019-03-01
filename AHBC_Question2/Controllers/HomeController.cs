7b00f0a059ab3532617af48fe63a5a8ddb23ff84
swurst6@gmail.com <swurst6@gmail.com>
2019-02-28 19:43:39 -0500

    Question2_Assessment

--- AHBC_Question2/Controllers/HomeController.cs
+++ AHBC_Question2/Controllers/HomeController.cs
@@ -8,23 +8,27 @@ namespace AHBC_Question2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Welcome()
         {
+            var random = new Random();
+            var list = new List<string> { "Thank you for Visiting", "Enjoy the site!", "Glad to have you", "Rate us on Google" };
+            int index = random.Next(list.Count);
+
+            ViewBag.Phrase = list[index];
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Receipt()
         {
-            ViewBag.Message = "Your application description page.";
+            Random rng = new Random();
+            ViewBag.Random = rng.Next(20000, 50000);
+            
+
+
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
